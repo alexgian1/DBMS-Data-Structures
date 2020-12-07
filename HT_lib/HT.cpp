a0f2403eded62c9b0e188d584062df68225ac9e2
Alex <avg555@hotmail.gr>
2020-12-07 15:11:29 +0200

    Add files via upload

--- HT_lib/HT.cpp
+++ HT_lib/HT.cpp
@@ -43,13 +43,6 @@ int HT_CreateIndex(char* filename, char attrType, char* attrName, int attrLength
     setNextBlock(blockFile,lastAllocatedBlock,-1); //set the next block of the last allocated block to be NULL
     
     setNextBlock(blockFile,0,lastAllocatedBlock);  //set the next block of HT_INFO block to be the last allocated block
-    
-    /*
-    cout << "First block is: " << HTinfoBlock << endl;
-    void* lastBlockBytes = static_cast<char*>(HTinfoBlock) + 512 -sizeof(int);  //point to the last 8 bytes (size of int) of the block
-    cout << "Next block saved at: " << lastBlockBytes << endl;
-    memcpy(lastBlockBytes, &lastAllocatedBlock, sizeof(int));  //copy the index of the allocated block to the last 8 bytes of HT_info block
-    */
 
     if (BF_WriteBlock(blockFile,0) < 0) return -1;
 
@@ -65,8 +58,8 @@ HT_info* HT_OpenIndex(char* filename){
     void* HTinfoBlock;
     if (BF_ReadBlock(blockFile,0,&HTinfoBlock) < 0) return NULL;  //store HT_info block location to HTinfoBlock pointer (HT_info is block 0 of the file)
     
-    HT_info* infoPtr = (HT_info*) malloc(sizeof(HT_info)); //allocate an HT_info struct and assign a pointer to it
-    if (infoPtr == NULL) return NULL;  //If malloc could not allocate space for infoPtr return error
+    HT_info* infoPtr = new(HT_info); //allocate an HT_info struct and assign a pointer to it
+    if (infoPtr == NULL) return NULL;  //If new could not allocate space for infoPtr return error
     memcpy(infoPtr,HTinfoBlock,sizeof(HT_info));  //store HT_info block contents to the allocated struct which infoPtr points
 
     return infoPtr;  //return the location of the retrieved info
@@ -82,7 +75,7 @@ int HT_CloseIndex(HT_info* header_info){
 
 
 int HT_InsertEntry(HT_info header_info, Record record){
-    cout << "Number of blocks: " << BF_GetBlockCounter(header_info.fileDesc) << endl;
+    //cout << "Number of blocks: " << BF_GetBlockCounter(header_info.fileDesc) << endl;
     
     //Search for the entry 
     int hashKey = hashFunction(record.id, header_info.numBuckets);
@@ -96,33 +89,33 @@ int HT_InsertEntry(HT_info header_info, Record record){
 
     if (blockNumber == 0){  //if the current block is the HT_info block, go to the next block
         blockNumber = getNextBlock(header_info.fileDesc, 0);
-        cout << "Skipping header block 0" << endl;
+        //cout << "Skipping header block 0" << endl;
     }
     
 
     void* recordPtr;
     while (blockNumber != -1){   //While there is a block left to search
-        BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr);
-        cout << "Checking block " << blockNumber << endl;
+        if (BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr) == -1) return -1;
+        //cout << "Checking block " << blockNumber << endl;
         for(int i=1; i<=RECORDS_PER_BLOCK; i++){
             if (*static_cast<int*>(recordPtr) == 0){  //if no record is saved in this location, go sizeof(Record) bytes forward in the block file
-                cout << "   No record in record location " << i << endl;
+                //cout << "   No record in record location " << i << endl;
             }
             else{  //if there is a record saved in this location, check if it is the same as the one we are inserting
                 Record curRecord;
                 memcpy(&curRecord,recordPtr,sizeof(Record)); //copy the current record from block to curRecord variable
                 if (curRecord.id == record.id) {
-                    cout << "   Record with id " << curRecord.id << " already exists! Insertion failed." << endl;
+                    //cout << "   Record with id " << curRecord.id << " already exists! Insertion failed." << endl;
                     return -1;
                 }
-                cout << "   Record location "<< i <<" full." << endl;
+                //cout << "   Record location "<< i <<" full." << endl;
             }
             recordPtr = static_cast<Record*>(recordPtr) + 1;
         }
         blockNumber = getNextBlock(header_info.fileDesc, blockNumber); //Go to the next block
-        cout << "   Next block is: " << blockNumber << endl;
+        //cout << "   Next block is: " << blockNumber << endl;
     }
-    cout << "Record not found! Inserting..." << endl;
+    //cout << "Record not found! Inserting..." << endl;
 
     //Insert the entry 
     //Find the 1st empty record location
@@ -130,35 +123,35 @@ int HT_InsertEntry(HT_info header_info, Record record){
     blockNumber = stoi(blockNumberStr);  //block number now contains the location of the required block
     if (blockNumber == 0){  //if the current block is the HT_info block, go to the next block
         void* block;
-        BF_ReadBlock(header_info.fileDesc, 0, &block);
+        if (BF_ReadBlock(header_info.fileDesc, 0, &block) == -1) return -1;
         blockNumber = getNextBlock(header_info.fileDesc, 0);
     }
-    BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr);
+    if (BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr) == -1) return -1;
 
     while (blockNumber != -1){   //While there is a block left to search
-        BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr);
-        cout << "Checking block " << blockNumber << endl;
+        if (BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr) == -1) return -1;
+        //cout << "Checking block " << blockNumber << endl;
         for(int i=1; i<=RECORDS_PER_BLOCK; i++){
-            cout << "   Checking record location " << i << endl;
+            //cout << "   Checking record location " << i << endl;
             if (*static_cast<int*>(recordPtr) == 0){  //if no record is saved in this location
                 memcpy(recordPtr, &record, sizeof(Record));   //save the record
                 BF_WriteBlock(header_info.fileDesc,blockNumber);
-                cout << "Record inserted successfully!" << endl;
+                //cout << "Record inserted successfully!" << endl;
                 return 0;
             }
             recordPtr = static_cast<Record*>(recordPtr) + 1;  //go to the next record location
         }
         
         if (getNextBlock(header_info.fileDesc, blockNumber) == -1){  //if this block is the final and it is full generate a new
-            cout << "   Block full! Generating new...: " << blockNumber << endl;
-            BF_AllocateBlock(header_info.fileDesc);
+            //cout << "   Block full! Generating new...: " << blockNumber << endl;
+            if (BF_AllocateBlock(header_info.fileDesc) == -1) return -1;
             int lastAllocatedBlock = BF_GetBlockCounter(header_info.fileDesc) - 1;
             setNextBlock(header_info.fileDesc, blockNumber, lastAllocatedBlock);
             setNextBlock(header_info.fileDesc, lastAllocatedBlock, -1);  //set the next of the new block to be NULL
         }
-        else{        //Block full, but there is another block after that
-            cout << "   Block full! Next block is: " << blockNumber << endl;
-        }
+        //else{        //Block full, but there is another block after that
+            //cout << "   Block full! Next block is: " << blockNumber << endl;
+        //}
         blockNumber = getNextBlock(header_info.fileDesc, blockNumber); //Go to the next block
     }
     return -1;
@@ -179,28 +172,29 @@ int HT_DeleteEntry(HT_info header_info, void* value){
 
     if (blockNumber == 0){  //if the current block is the HT_info block, go to the next block
         blockNumber = getNextBlock(header_info.fileDesc, 0);
-        cout << "Skipping header block 0" << endl;
+        //cout << "Skipping header block 0" << endl;
     }
     
 
     void* recordPtr;
     while (blockNumber != -1){   //While there is a block left to search
-        BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr);
+        if (BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr) == -1) return -1;
         for(int i=1; i<=RECORDS_PER_BLOCK; i++){
             Record curRecord;
             memcpy(&curRecord,recordPtr,sizeof(Record)); //copy the current record from block to curRecord variable
             if (curRecord.id == id) {   //if the retrieved record is the required, delete it from the block 
-                cout << "Deleting : {" << curRecord.id << "," << curRecord.name << "," 
-                << curRecord.surname << "," << curRecord.address << "}"  << endl;
+                //cout << "Deleting : {" << curRecord.id << "," << curRecord.name << "," 
+                //<< curRecord.surname << "," << curRecord.address << "}"  << endl;
                 memset(recordPtr,0,sizeof(Record));   //set all bytes at the current record location to be 0
-                cout << "Success." << endl;
+                if (BF_WriteBlock(header_info.fileDesc,blockNumber) == -1) return -1;
+                //cout << "Success." << endl;
                 return 0;
             }
             recordPtr = static_cast<Record*>(recordPtr) + 1;
         }
         blockNumber = getNextBlock(header_info.fileDesc, blockNumber); //Go to the next block
     }
-    cout << "Record with id " << id << " not found!" << endl;
+    //cout << "Record with id " << id << " not found!" << endl;
     return -1;
 }
 
@@ -222,21 +216,21 @@ int HT_GetAllEntries(HT_info header_info, void* value){
 
     if (blockNumber == 0){  //if the current block is the HT_info block, go to the next block
         blockNumber = getNextBlock(header_info.fileDesc, 0);
-        cout << "Skipping header block 0" << endl;
+        //cout << "Skipping header block 0" << endl;
         blocksExpanded++;
     }
     
 
     void* recordPtr;
     while (blockNumber != -1){   //While there is a block left to search
-        BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr);
+        if (BF_ReadBlock(header_info.fileDesc, blockNumber, &recordPtr) == -1) return -1;
         for(int i=1; i<=RECORDS_PER_BLOCK; i++){
             Record curRecord;
             memcpy(&curRecord,recordPtr,sizeof(Record)); //copy the current record from block to curRecord variable
             if (curRecord.id == id) {  //if the retrieved record is the required, search is completed
-                cout << "Record with id " << id << " found!" << endl;
-                cout << "{" << curRecord.id << "," << curRecord.name << "," << curRecord.surname << "," << curRecord.address << "}"  << endl;
-                cout << "Expanded " << blocksExpanded << " blocks." << endl;
+                //cout << "Record with id " << id << " found!" << endl;
+                //cout << "{" << curRecord.id << "," << curRecord.name << "," << curRecord.surname << "," << curRecord.address << "}"  << endl;
+                //cout << "Expanded " << blocksExpanded << " blocks." << endl;
                 return blocksExpanded;
             }
             recordPtr = static_cast<Record*>(recordPtr) + 1;
@@ -244,7 +238,7 @@ int HT_GetAllEntries(HT_info header_info, void* value){
         blockNumber = getNextBlock(header_info.fileDesc, blockNumber); //Go to the next block
         blocksExpanded++;
     }
-    cout << "Record with id " << id << " not found!" << endl;
+    //cout << "Record with id " << id << " not found!" << endl;
     return -1;
 }
 
@@ -372,7 +366,7 @@ void Read_From_File(HT_info header_info, string recordsFile){
     		strcpy(rec.address,address.c_str());
 
 
-    		HT_InsertEntry(header_info,rec);
+    		if (HT_InsertEntry(header_info,rec) != -1) cout << "Record with id " << rec.id << " inserted successfully!" << endl;
  		}
 	}
 	myReadFile.close();
--- HT_lib/main.cpp
+++ HT_lib/main.cpp
@@ -8,38 +8,53 @@ using namespace std;
 
 int main(){
 
-    if (HT_CreateIndex("HashTable1",'i',"Attribute",5,300) == 0){
+    if (HT_CreateIndex("HashTable1",'i',"Attribute",5,100) == 0){
         cout << "File created sucessfully!" << endl;
     }
 
     HT_info* HT1_info = HT_OpenIndex("HashTable1");
-    
-    if (HT1_info != NULL){
-        cout << "HT1 fileDesc: " << HT1_info->fileDesc << endl;
-        cout << "HT1 attrType: " << HT1_info->attrType << endl;
-        cout << "HT1 attrName: " << HT1_info->attrName << endl;
-        cout << "HT1 attrLength: " << HT1_info->attrLength << endl;
-        cout << "HT1 numBuckets: " << HT1_info->numBuckets << endl;
-        cout << "HT1 index: " << HT1_info->indexFilename << endl;
-    }
 
     Read_From_File(*HT1_info,"records5K.txt");
+
 
     cout << "----------------------------- DELETE TEST -----------------------------" << endl;
 
     int valuesToDelete[4] = {150,12,2400,54445};
-    HT_DeleteEntry(*HT1_info, &valuesToDelete[0]);
-    HT_DeleteEntry(*HT1_info, &valuesToDelete[1]);
-    HT_DeleteEntry(*HT1_info, &valuesToDelete[2]);
-    HT_DeleteEntry(*HT1_info, &valuesToDelete[2]);  //Delete already delete entry
-    HT_DeleteEntry(*HT1_info, &valuesToDelete[3]);  //Delete non-existent entry
+
+    if (HT_DeleteEntry(*HT1_info, &valuesToDelete[0]) != -1) cout << "Record with id " << valuesToDelete[0] << " deleted successfully!" << endl;
+    else cout<< "Error deleting record with id " << valuesToDelete[0] << endl;
+
+    if (HT_DeleteEntry(*HT1_info, &valuesToDelete[1]) != -1) cout << "Record with id " << valuesToDelete[1] << " deleted successfully!" << endl;
+    else cout<< "Error deleting record with id " << valuesToDelete[1] << endl;
+
+    if (HT_DeleteEntry(*HT1_info, &valuesToDelete[2]) != -1) cout << "Record with id " << valuesToDelete[2] << " deleted successfully!" << endl;
+    else cout<< "Error deleting record with id " << valuesToDelete[2] << endl;
+
+    //Delete already deleted entry
+    if (HT_DeleteEntry(*HT1_info, &valuesToDelete[2]) != -1) cout << "Record with id " << valuesToDelete[2] << " deleted successfully!" << endl;
+    else cout<< "Error deleting record with id " << valuesToDelete[2] << endl; 
+    
+    //Delete non-existent entry
+    if (HT_DeleteEntry(*HT1_info, &valuesToDelete[3]) != -1) cout << "Record with id " << valuesToDelete[3] << " deleted successfully!" << endl;
+    else cout<< "Error deleting record with id " << valuesToDelete[3] << endl;   
+
 
     cout << "----------------------------- SEARCH TEST -----------------------------" << endl;
 
     int valuesToSearch[3] = {500,150,99999};
-    HT_GetAllEntries(*HT1_info, &valuesToSearch[0]);
-    HT_GetAllEntries(*HT1_info, &valuesToSearch[1]);  //Search deleted entry
-    HT_GetAllEntries(*HT1_info, &valuesToSearch[2]);  //Search non-exitent entry
+    
+    if (HT_GetAllEntries(*HT1_info, &valuesToSearch[0]) != -1) cout << "Record with id " << valuesToSearch[0] << " found!" << endl;
+    else cout<< "Record with id " << valuesToSearch[0] << " not found!" << endl;   
+    
+    //Search deleted entry
+    if (HT_GetAllEntries(*HT1_info, &valuesToSearch[1]) != -1) cout << "Record with id " << valuesToSearch[1] << " found!" << endl;
+    else cout<< "Record with id " << valuesToSearch[1] << " not found!" << endl;     
+
+    //Search non-exitent entry
+    if (HT_GetAllEntries(*HT1_info, &valuesToSearch[2]) != -1) cout << "Record with id " << valuesToSearch[2] << " found!" << endl;
+    else cout<< "Record with id " << valuesToSearch[2] << " not found!" << endl;    
+
+    //-------------------------------------------------------------------------------------------------------------------------------//
 
     hashStatistics("HashTable1");
 
